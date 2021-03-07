dbaf259c137e2daeb541e4019b36cee54253f55d
WebMakaka <marley.nodejs@gmail.com>
2021-03-07 07:39:40 +0300

    Add absolute path import in node project

--- app/api/src/index.js
+++ app/api/src/index.js
@@ -1,11 +1,11 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import path from 'path';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
-import orderRouter from './routers/orderRouter.js';
-import uploadRouter from './routers/uploadRouter.js';
+import orderRouter from 'routers/orderRouter.js';
+import productRouter from 'routers/productRouter.js';
+import uploadRouter from 'routers/uploadRouter.js';
+import userRouter from 'routers/userRouter.js';
 
 dotenv.config();
 
@@ -22,23 +22,23 @@ app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-app.get('/api/config/paypal', (req, res) => {
+app.get('/api/config/paypal', (_req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-app.get('/api/config/google', (req, res) => {
+app.get('/api/config/google', (_req, res) => {
   res.send(process.env.GOOGLE_API_KEY || '');
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
+app.get('*', (_req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   res.status(500).send({ message: err.message });
 });
 
